54615b44bc1c0d1613e60c78e1d18d0faaf46dcb
Carl Christian Tegner <cctegner@hotmail.com>
2026-01-05 14:56:39 +0100

    initial

---
+++ app/src/main/java/com/example/budgetapp_grouptwo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.budgetapp_grouptwo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.budgetapp_grouptwo.ui.theme.BudgetApp_GroupTwoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BudgetApp_GroupTwoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BudgetApp_GroupTwoTheme {
+        Greeting("Android")
+    }
+}
