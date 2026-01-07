e61c592c95fe52b101c62bc0de8b7a4627030c09
Carl Christian Tegner <cctegner@hotmail.com>
2026-01-07 15:21:56 +0100

    ved ikke

---
+++ app/src/main/java/com/example/budgetapp_grouptwo/ui/screens/BudgetScreen.kt
@@ -0,0 +1,2 @@
+package com.example.budgetapp_grouptwo.ui.screens
+
--- app/src/main/java/com/example/budgetapp_grouptwo/viewmodel/BudgetViewModel.kt
+++ app/src/main/java/com/example/budgetapp_grouptwo/viewmodel/BudgetViewModel.kt
@@ -1,6 +1,22 @@
-package com.example.budgetapp_grouptwo.viewmodel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.budgetapp_grouptwo.model.RegularCashFlow
+
 
 class BudgetViewModel: ViewModel() {
-    // Instans til klassen
-    var myCurrentBudget = RegularCashFlow(earnings = 0.0, expenses = 0.0)
+    // Instans til klassen altså en 'state', for at Compose ved hvornår der skal ske noget
+    var cashFlow by mutableStateOf(RegularCashFlow(id = "1"))
+        private set
+
+    fun updateEarnings(newAmount:String) {
+        val amount = newAmount.toDoubleOrNull() ?: 0.0
+        cashFlow = cashFlow.copy(earnings = amount)
+    }
+
+    fun updateExpenses(newAmount:String) {
+        val amount = newAmount.toDoubleOrNull() ?: 0.0
+        cashFlow = cashFlow.copy(expenses = amount)
+    }
 }
