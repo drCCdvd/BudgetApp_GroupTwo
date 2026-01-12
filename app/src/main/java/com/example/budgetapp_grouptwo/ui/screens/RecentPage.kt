eb0f6b9250d0eab0cb665edbdb4489815a64cfab
Carl Christian Tegner <cctegner@hotmail.com>
2026-01-12 15:12:43 +0100

    Opdatering til seneste

--- app/src/main/java/com/example/budgetapp_grouptwo/ui/screens/RecentPage.kt
+++ app/src/main/java/com/example/budgetapp_grouptwo/ui/screens/RecentPage.kt
@@ -1,7 +1,10 @@
 package com.example.budgetapp_grouptwo.ui.screens
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +24,30 @@ fun RecentPage(
     ) {
         NavigationMenu(navController)
         Text ("Recent Page")
+        Test()
     }
+}
+
+
+@Composable
+fun Test(
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(2.dp, Color.Red)
+    ) {
+        Column(
+            modifier = Modifier.border(2.dp, Color.Blue)
+        ) {
+            Text("23. Okt")
+            Text("Mobilepay (Far)")
+        }
+        Text("45kr.")
+        Text("Fjern")
+    }
+
 }
 
 
